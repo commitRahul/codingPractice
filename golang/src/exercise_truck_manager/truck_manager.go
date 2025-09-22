d69651f8db1d830fcb0dc14f885e8f05775986ef
Rahul Kumar <rahul623ee@gmail.com>
2025-09-22 22:20:16 +0530

    exercise of truck manager

---
+++ golang/src/exercise_truck_manager/truck_manager.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+)
+
+var ErrTruckNotFound = errors.New("truck not found")
+
+type FleetManager interface {
+	AddTruck(id string, cargo int) error
+	GetTruck(id string) (*Truck, error)
+	RemoveTruck(id string) error
+	UpdateTruckCargo(id string, cargo int) error
+}
+
+type Truck struct {
+	ID    string
+	Cargo int
+}
+
+type truckManager struct {
+	trucks map[string]*Truck
+}
+
+func NewTruckManager() truckManager {
+	return truckManager{
+		trucks: make(map[string]*Truck),
+	}
+}
+
+func (t *truckManager) AddTruck(id string, cargo int) {
+	t.trucks[id] = &Truck{ID: id, Cargo: cargo}
+}
+
+func (t *truckManager) GetTruck(id string) (*Truck, error) {
+	truck, exists := t.trucks[id]
+	if !exists {
+		return nil, ErrTruckNotFound
+	}
+	return truck, nil
+}
+
+func (t *truckManager) RemoveTruck(id string) {
+	delete(t.trucks, id)
+}
+
+func (t *truckManager) UpdateTruckCargo(id string, cargo int) {
+	if truck, exists := t.trucks[id]; exists {
+		truck.Cargo = cargo
+	}
+}
+
+func main() {
+	// Example usage
+	manager := NewTruckManager()
+	manager.AddTruck("1", 100)
+	truck, err := manager.GetTruck("1")
+	if err != nil {
+		panic(err)
+	}
+	println("Truck ID:", truck.ID, "Cargo:", truck.Cargo)
+}
